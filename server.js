7bbbf0d326dd0c80f8f7404125c0b2592a05c83e
Satyam Mishra <mishrasatyamsam@gmail.com>
2022-01-22 09:49:41 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const ejsLayout = require('express-ejs-layouts');
+const path = require('path');
+
+
+
+
+app.get("/",(req,res)=>{
+    // res.send("hello World!")
+    res.render("home");
+})
+
+//set the templates
+app.use(ejsLayout); //to use the layouts
+app.set('views',path.join(__dirname,'/resources/views')); //mention where the views present
+app.set('view engine','ejs'); //which template using
+
+
+
+
+
+const PORT =process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`server started at port ${PORT}`);
+})
